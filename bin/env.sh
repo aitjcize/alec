48d6ddbbebc5de0fb79d37364b0f74c5ed4a749c
Cheng-Yi Chiang <cychiang@chromium.org>
2017-12-11 13:36:03 +0000

    env.sh: Let env.sh load different configs
    source bin/env.sh abc
    to load bin/config_abc.sh
    
    source bin/env.sh
    to load bin/config.sh

--- bin/env.sh
+++ bin/env.sh
@@ -9,8 +9,16 @@ fi
 export PATH=$GITROOT/bin:$PATH
 export PYTHONPATH=$GITROOT:$PYTHONPATH
 
-CONFIG_FILE=$GITROOT/bin/config.sh
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, use default config.sh"
+    CONFIG_FILE=$GITROOT/bin/config.sh
+else
+    CONFIG_FILE=$GITROOT/bin/config_$1.sh
+    echo "Use config $CONFIG_FILE"
+fi
 
 if [ -e $CONFIG_FILE ]; then
+  echo "source $CONFIG_FILE"
   source $CONFIG_FILE
 fi
