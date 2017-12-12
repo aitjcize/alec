d855316a0f5fd433641c198594a03043fac3c1de
Cheng-Yi Chiang <cychiang@chromium.org>
2017-12-12 19:56:11 +0000

    db.sql: Also show buy order
    It will be useful to see both buy orders and sell orders.

--- alec/scripts/db.sql
+++ alec/scripts/db.sql
@@ -1,16 +1,19 @@
 CREATE VIEW today
 AS
-SELECT DATE(datetime(timestamp, 'unixepoch')) as day, count(*), sum(price*amount)
+SELECT DATE(datetime(timestamp, 'unixepoch')) as day, side, count(*), sum(price*amount)
 FROM executed_orders
-WHERE day = DATE('now') AND side = 'sell';
+WHERE day = DATE('now')
+GROUP BY side;
+
 
 CREATE VIEW today_coin
 AS
-SELECT DATE(datetime(timestamp, 'unixepoch')) as day, symbol, count(*), sum(price*amount) AS value
+SELECT DATE(datetime(timestamp, 'unixepoch')) as day, side, symbol, count(*), sum(price*amount)
 FROM executed_orders
-WHERE day = DATE('now') AND side = 'sell'
-GROUP BY symbol
-ORDER BY value;
+WHERE day = DATE('now')
+GROUP BY symbol, side
+ORDER BY symbol, side;
+
 
 CREATE VIEW history
 AS
